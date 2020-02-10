0a48d09c54d20723d13d5b07a2e0c5f0340ba163
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 01:59:09 +0300

    Scripted auto-commit on change (2020-02-11 01:59:09) by gitwatch.sh

--- src/main/java/UtilitiesCSV.java
+++ src/main/java/UtilitiesCSV.java
@@ -4,4 +4,8 @@ public class UtilitiesCSV {
     private static final int DELAYED = 18;
     private static final int AIRPORT_KEY = 0;
     private static final int FLIGHT_KEY = 1;
+    private static final int FIRST_ITEM = 0;
+
+    
+
 }
