ceddfe2ae68880a35096555b9706080285076b0e
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 02:30:05 +0300

    Scripted auto-commit on change (2020-02-11 02:30:05) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -1,2 +1,10 @@
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
 public class AirportReducer {
+    
 }
+
