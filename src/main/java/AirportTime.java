341c6c62804f7f48155c575c96b81cd75ca060a6
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 02:28:27 +0300

    Scripted auto-commit on change (2020-02-11 02:28:27) by gitwatch.sh

--- src/main/java/AirportTime.java
+++ src/main/java/AirportTime.java
@@ -28,6 +28,7 @@ public class AirportTime {
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(Text.class);
     job.setNumReduceTasks(2);
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
 
 
 
