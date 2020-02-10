0202829e51c5896288fba8d4a65d45afa932c14f
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 02:21:33 +0300

    Scripted auto-commit on change (2020-02-11 02:21:33) by gitwatch.sh

--- src/main/java/AirportGroupingComparator.java
+++ src/main/java/AirportGroupingComparator.java
@@ -1,5 +1,7 @@
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 public class AirportGroupingComparator {
-    
+    AirportGroupingComparator() {
+        
+    }
 }
