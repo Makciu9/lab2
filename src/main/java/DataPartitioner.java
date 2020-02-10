dea0faf6eae6a57c264b61cad8bd8b1c44a3de09
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 02:45:12 +0300

    Scripted auto-commit on change (2020-02-11 02:45:12) by gitwatch.sh

--- src/main/java/DataPartitioner.java
+++ src/main/java/DataPartitioner.java
@@ -3,5 +3,5 @@ import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 public class DataPartitioner extends HashPartitioner<AirportWritableComparable, Text> {
     @Override
     public int getPartition(AirportWritableComparable key, Text value, int numReduceTasks) {
-
+        return (Integer.valueOf(key.getAirportID()).hashCode() & Integer.MAX_VALUE) % numReduceTasks;
 }
