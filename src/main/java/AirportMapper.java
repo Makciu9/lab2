b38eb869d7842e89ad1506897f8d087463cc4c6a
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 02:23:34 +0300

    Scripted auto-commit on change (2020-02-11 02:23:34) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -1,2 +1,7 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
 public class AirportMapper {
 }
