e8140f9105c937564db8477d87c024d576397e09
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 02:48:23 +0300

    Scripted auto-commit on change (2020-02-11 02:48:23) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -9,7 +9,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
     private static final int KEY_DATA = 1;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        
+        MapTemplate.use(value, context, KEY_DATA, AIRPORT_ID, DELAYED);
     }
 }
 
