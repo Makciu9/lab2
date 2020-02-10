2e4d8abd17517ec8113bb6c0b5e1ef684bef1c72
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 02:49:08 +0300

    Scripted auto-commit on change (2020-02-11 02:49:08) by gitwatch.sh

--- src/main/java/MapTemplate.java
+++ src/main/java/MapTemplate.java
@@ -3,5 +3,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 public class MapTemplate {
-    
+    public static void use(Text value, Mapper.Context context, int keyData, int airportId, int delayed) throws IOException, InterruptedException {
+        
+    }
+
 }
