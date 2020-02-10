051266c8528005d2c0c74e3d6b1db3b7d1c593d9
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 02:48:02 +0300

    Scripted auto-commit on change (2020-02-11 02:48:02) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
-    
+    private static final int AIRPORT_ID = 14;
+    private static final int DELAYED = 18;
+    private static final int KEY_DATA = 1;
 }
 
