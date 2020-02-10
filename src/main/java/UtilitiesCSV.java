63f14530ba9fec993c29dc4335bba9e1112c8a7a
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-11 02:08:23 +0300

    Scripted auto-commit on change (2020-02-11 02:08:23) by gitwatch.sh

--- src/main/java/UtilitiesCSV.java
+++ src/main/java/UtilitiesCSV.java
@@ -42,10 +42,10 @@ public class UtilitiesCSV {
         }
     }
     private static String[] filterFlightData(String[] parsed) {
-        if ((parsed[FIRST_ITEM].equals("\"YEAR\""))||
-        (parsed[CANCELED].equals("1.00"))||
-        parsed[DELAYED].equals("0.00"))) {
+        if ((parsed[FIRST_ITEM].equals("\"YEAR\""))|| (parsed[CANCELED].equals("1.00"))|| parsed[DELAYED].equals("0.00")) {
             return EMPTY;
     } else {
+            return parsed;
+        }
 
 }
